d839d14b0ffd75bebac7f07303f64f78afc690a9
m <mahhov1@gmail.com>
2017-11-04 12:14:47 -0700

    port

--- cardSandboxServer.js
+++ cardSandboxServer.js
@@ -12,8 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.text());
 
-app.listen(8080, () => {
-    console.log('express listen 8080')
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('express listen', port)
 });
 
 app.use((req, res, next) => {
